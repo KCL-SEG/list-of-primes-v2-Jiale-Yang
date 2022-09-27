35dc89ee81018bb776972f0fc19998671ab978ce
Jiale Yang <jiale.2.yang@kcl.ac.uk>
2022-09-27 12:31:34 +0100

    primes-v2

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+	list = []
+	currentNum = 2
+    
+	if number_of_primes <= 0:
+		raise ValueError
+
+	while len(list) < number_of_primes:
+		prime = True
+		for i in range(2, currentNum):
+			if currentNum % i == 0:
+				prime = False
+	
+		if prime:
+			list.append(currentNum)
+		currentNum += 1
+			
+			
+	return list
